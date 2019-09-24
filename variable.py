49b7447d203bb0a1e3e324976785f11df468fc9e
jacob-bonner <54957118+jacob-bonner@users.noreply.github.com>
2019-09-24 14:45:59 -0400

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Created by: Jacob Bonner
+# Created on: September 2019
+# This program is some experimentation of local and global variables
+
+# global variables
+variable_x = 25
+
+
+def local_variable():
+    # This function demonstrates local variables
+    
+    variable_x = 
